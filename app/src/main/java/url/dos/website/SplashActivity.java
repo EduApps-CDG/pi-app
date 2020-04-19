64d724ff8e727e2c601a512a03441d351ba31cc9
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2020-04-19 18:52:48 -0300

    upload via upload.sh

---
+++ app/src/main/java/url/dos/website/SplashActivity.java
@@ -0,0 +1,12 @@
+package url.dos.website;
+import android.app.*;
+import android.os.*;
+
+public class SplashActivity extends Activity {
+
+	@Override protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+	}
+	
+}
