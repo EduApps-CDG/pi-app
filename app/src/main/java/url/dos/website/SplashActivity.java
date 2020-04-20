0986d950d51245afb3ee0d89b64320e5dbfdab4f
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2020-04-20 14:38:14 -0300

    upload via upload.sh

--- app/src/main/java/url/dos/website/SplashActivity.java
+++ app/src/main/java/url/dos/website/SplashActivity.java
@@ -1,6 +1,7 @@
 package url.dos.website;
 import android.app.*;
 import android.os.*;
+import url.dos.website.util.*;
 
 public class SplashActivity extends Activity {
 
@@ -8,5 +9,11 @@ public class SplashActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
 	}
-	
+
+	@Override public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus = true) {
+			AndroidUtils.setImmersiveMode(this,true);
+		}
+	}
 }
